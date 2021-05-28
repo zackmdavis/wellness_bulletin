e3cb6fbb855456e580ad791a52297960fb0513ad
Zack M. Davis <code@zackmdavis.net>
2021-05-27 22:12:43 -0700

    post to Twitter
    https://twitter.com/zackmdavis/status/1398144745306689537

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::time::Duration;
 
 use reqwest::header;
+use egg_mode::tweet::DraftTweet;
 
 mod activity;
 mod sleep;
@@ -10,12 +11,32 @@ use activity::ActivityResponse;
 use sleep::SleepResponse;
 
 
-fn read_access_token() -> Result<String, std::io::Error> {
+fn read_oura_access_token() -> Result<String, std::io::Error> {
     fs::read_to_string(".oura_access_token")
 }
 
+fn read_secret_file_data(filename: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let file_content = fs::read_to_string(filename)?;
+    Ok(file_content.trim_end().to_string())
+}
+
+fn construct_twitter_token() -> Result<egg_mode::Token, Box<dyn std::error::Error>> {
+    let consumer_key = read_secret_file_data(".twitter_consumer_key")?;
+    let consumer_secret = read_secret_file_data(".twitter_consumer_secret")?;
+    let access_token_key = read_secret_file_data(".twitter_access_token_key")?;
+    let access_token_secret = read_secret_file_data(".twitter_access_token_secret")?;
+    let consumer_token = egg_mode::KeyPair::new(consumer_key, consumer_secret);
+    let access_token = egg_mode::KeyPair::new(access_token_key, access_token_secret);
+    let token = egg_mode::Token::Access {
+        consumer: consumer_token,
+        access: access_token,
+    };
+    Ok(token)
+}
+
+
 fn oura_request(endpoint: &str) -> Result<reqwest::blocking::Response, Box<dyn std::error::Error>> {
-    let token_file_content = read_access_token()?;
+    let token_file_content = read_oura_access_token()?;
     let access_token = token_file_content.trim_end();
     println!("{:?}", access_token);
 
@@ -56,7 +77,10 @@ fn bulletin(wellness_domain: WellnessDomain) -> Result<String, Box<dyn std::erro
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello wellness world!");
-    println!("{:?}", bulletin(WellnessDomain::Activity));
+    let token = construct_twitter_token().expect("I hope this works");
+    let tweet = DraftTweet::new("Hello egg-mode World!");
+    tweet.send(&token).await.unwrap();
 }
